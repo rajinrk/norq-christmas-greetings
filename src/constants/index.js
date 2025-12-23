fdd494a9530e4f0f338be80207c8be139a740cdd
Rajin-norq <rajin.radhakrishnan@norq.in>
2025-12-23 12:41:06 +0530

    time updated to 1

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,8 @@
-export const defaultValues = {  year: 2025,
+export const defaultValues = {
+  year: 2025,
   month: 11,
   day: 23,
-  hour: 12,
-  minute: 30,
+  hour: 13,
+  minute: 0,
   seconds: 0,
 };
